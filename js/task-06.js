193a2062b71d94144c24894f564ddd7eea68263f
Ilya Chachua <cacuaila@gmail.com>
2022-01-14 09:59:15 +0200

    1111

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,19 @@
-const input = document.querySelector('#validation-input');
-const dataLen = input.getAttribute('data-length');
-input.addEventListener('focusout', () => {
-  if (input.value.length == dataLen) {
-    input.classList.remove('invalid');
-    input.classList.add('valid');
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('blur', event => {
+  if (event.currentTarget.value.length == 6) {
+    console.log(1);
+
+    inputEl.classList.add('valid'); //}
+    return;
   } else {
-    input.classList.remove('valid');
-    input.classList.add('invalid');
+    console.log(2);
+
+    inputEl.classList.add('invalid');
   }
+});
+
+inputEl.addEventListener('focus', event => {
+  inputEl.classList.remove('invalid');
+  inputEl.classList.remove('valid');
 });
